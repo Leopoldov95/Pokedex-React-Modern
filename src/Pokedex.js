63996a827bb16988175293dc0d8ecdb88bfbce79
Leopoldov95 <leo386_ortega@mail.fresnostate.edu>
2021-06-08 07:17:33 -0500

    still working on making app use hooks

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import Pokecard from "./Pokecard";
@@ -15,33 +15,23 @@ const API_URl = "https://pokeapi.co/api/v2/pokemon/";
 const IMG_URL = "https://img.pokemondb.net/sprites/home/normal/";
 const POKE_FORMS = "https://pokeapi.co/api/v2/pokemon-species/";
 
-class Pokedex extends Component {
-  static defaultProps = {
-    default: 151,
-  };
-  constructor(props) {
-    super(props);
-    this.state = {
-      pokemon: [], // all pokemon will be stored here
+function Pokedex() {
+  const defaultCall = 151;
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [displayPokedex, setDisplayPokedex] = useState(true);
+  const [currPokemon, setCurrPokemon] = useState([]);
+
+  /* pokemon: [], // all pokemon will be stored here
       loading: false,
       displayPokedex: true,
-      currPokemon: [],
-    };
-    this.displayPokemon = this.displayPokemon.bind(this);
-    this.getPokemon = this.getPokemon.bind(this);
-    this.handleInfo = this.handleInfo.bind(this);
-    this.getForms = this.getForms.bind(this);
-    this.handleInput = this.handleInput.bind(this);
-    this.closeAutocomplete = this.closeAutocomplete.bind(this);
-    this.handleMenuBtn = this.handleMenuBtn.bind(this);
-  }
+      currPokemon: [], */
 
-  componentDidMount() {
-    // just want to generate intial display here
-    this.getPokemon(0, this.props.default);
-  }
+  useEffect(() => {
+    getPokemon(0, defaultCall);
+  }, []);
 
-  async getPokemon(start, end) {
+  const getPokemon = async (start, end) => {
     try {
       let res = await axios.get(`${API_URl}?limit=${end}`);
 
@@ -64,26 +54,30 @@ class Pokedex extends Component {
           img: `${IMG_URL}${name}.png`,
         });
       }
-      this.setState((st) => ({
+      setLoading(false);
+      setDisplayPokedex(true);
+      setPokemon([...pokemon]);
+      /* this.setState((st) => ({
         loading: false,
         displayPokedex: true,
         pokemon: [...st.pokemon, ...pokemon],
-      }));
+      })); */
     } catch (err) {
       alert(err);
     }
-  }
+  };
 
-  displayPokemon(start, end) {
-    this.setState({ pokemon: [] });
+  const displayPokemon = (start, end) => {
+    //this.setState({ pokemon: [] });
+    setPokemon([]);
 
-    this.getPokemon(start, end);
+    getPokemon(start, end);
     if (document.querySelector(".myLinks").classList.contains("menu-active")) {
       document.querySelector(".myLinks").classList.remove("menu-active");
     }
-  }
+  };
 
-  handleMenuBtn() {
+  const handleMenuBtn = () => {
     const x = document.querySelector(".menu-toggle");
     x.classList.toggle(".active");
     if (x.classList.contains(".active")) {
@@ -92,31 +86,31 @@ class Pokedex extends Component {
     } else {
       document.querySelector(".Pokedex-dropdown-items ").style.display = "none";
     }
-  }
+  };
 
-  closeFormSel() {
+  const closeFormSel = () => {
     if (document.querySelector(".Pokedex-dropdown-items")) {
       document.querySelector(".Pokedex-dropdown-items").style.display = "none";
     }
-  }
+  };
 
-  closeAutocomplete() {
+  const closeAutocomplete = () => {
     if (document.querySelector(".Autocomplete-list")) {
       document.querySelector(".Autocomplete-list").style.display = "none";
       document.querySelector(".form-text").value = "";
     }
-  }
+  };
 
-  async getPokeInfo(url) {
+  const getPokeInfo = async (url) => {
     try {
       let res = await axios.get(url);
       return res.data;
     } catch (err) {
       alert(err);
     }
-  }
+  };
 
-  async getForms(forms) {
+  const getForms = async (forms) => {
     let varData = [];
     let desc;
     let res = await axios.get(`${POKE_FORMS}${forms}/`);
@@ -138,11 +132,11 @@ class Pokedex extends Component {
     // these lines of code only trigger if above conditions have not been met
     varData.push(varieties[0].pokemon);
     return { evolution_chain, desc, varData };
-  }
+  };
 
-  async handleInfo(info, speciesURL) {
-    this.closeAutocomplete();
-    this.closeFormSel();
+  const handleInfo = async (info, speciesURL) => {
+    closeAutocomplete();
+    closeFormSel();
     let data = [];
     const {
       name,
@@ -154,10 +148,10 @@ class Pokedex extends Component {
       species,
       stats,
       sprites,
-    } = await this.getPokeInfo(info);
+    } = await getPokeInfo(info);
 
     // left off here, handle the addtional information!
-    let { varData, desc, evolution_chain } = await this.getForms(speciesURL);
+    let { varData, desc, evolution_chain } = await getForms(speciesURL);
 
     data.push({
       name,
@@ -173,72 +167,69 @@ class Pokedex extends Component {
       desc,
       evolution_chain,
     });
-    this.setState({
+    setDisplayPokedex(false);
+    setCurrPokemon(data);
+    /* this.setState({
       displayPokedex: false,
       currPokemon: data,
-    });
-  }
+    }); */
+  };
 
-  handleInput(e) {
+  const handleInput = (e) => {
     if (
       e.target.className !== "Autocomplete" ||
       e.target.className !== "Listpokemon"
     ) {
-      this.closeAutocomplete();
+      closeAutocomplete();
     }
-  }
+  };
 
-  render() {
-    let generatePokemon = this.state.pokemon.map((p) => (
-      <Pokecard
-        handleInfo={() => this.handleInfo(p.info, p.id)}
-        type={TypeInfo[p.id - 1]}
-        name={p.name}
-        info={p.info}
-        id={p.id}
-        img={p.img}
-        key={p.id}
-      />
-    ));
+  let generatePokemon = pokemon.map((p) => (
+    <Pokecard
+      handleInfo={() => handleInfo(p.info, p.id)}
+      type={TypeInfo[p.id - 1]}
+      name={p.name}
+      info={p.info}
+      id={p.id}
+      img={p.img}
+      key={p.id}
+    />
+  ));
 
-    let currentPokemon = this.state.currPokemon[0];
+  let currentPokemon = { currPokemon }[0];
 
-    return (
-      <div className="Pokedex" onClick={this.handleInput}>
-        <Pokenav
-          displayPokemon={this.displayPokemon}
-          handleInfo={this.handleInfo}
-        />
+  return (
+    <div className="Pokedex" onClick={handleInput}>
+      <Pokenav displayPokemon={displayPokemon} handleInfo={handleInfo} />
 
-        {this.state.displayPokedex ? (
-          <div>
-            {/*   <img src="./pokemon-types/bug.png" /> */}
-            <div className="Pokedex-pokemon">{generatePokemon}</div>
-          </div>
-        ) : (
-          <RenderPokeInfo
-            handleMenuBtn={this.handleMenuBtn}
-            handleInfo={this.handleInfo}
-            currentPokemon={currentPokemon}
-          />
-        )}
-        <div className="Pokedex-footer">
-          <p>
-            created by{" "}
-            <a href="https://github.com/Leopoldov95" target="#">
-              Leopoldo Ortega
-            </a>
-          </p>
-          <p>
-            powered by{" "}
-            <a href="https://pokeapi.co/" target="#">
-              Pokeapi
-            </a>
-          </p>
+      {displayPokedex ? (
+        <div>
+          {/*   <img src="./pokemon-types/bug.png" /> */}
+          <div className="Pokedex-pokemon">{generatePokemon}</div>
         </div>
+      ) : (
+        <RenderPokeInfo
+          handleMenuBtn={handleMenuBtn}
+          handleInfo={handleInfo}
+          currentPokemon={currentPokemon}
+        />
+      )}
+      <div className="Pokedex-footer">
+        <p>
+          created by{" "}
+          <a href="https://github.com/Leopoldov95" target="#">
+            Leopoldo Ortega
+          </a>
+        </p>
+        <p>
+          powered by{" "}
+          <a href="https://pokeapi.co/" target="#">
+            Pokeapi
+          </a>
+        </p>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Pokedex;
